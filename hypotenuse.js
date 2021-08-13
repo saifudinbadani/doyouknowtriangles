c3bec6744deba79bcced2bd8ea2e7caa8c2cd0a1
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-13 18:04:31 +0800

    negative number handling for hypotenuse values

--- hypotenuse.js
+++ hypotenuse.js
@@ -5,9 +5,14 @@ var output = document.querySelector('#output');
 
 
 submitButton.addEventListener('click', function checkHypotenuse() {
-    var sumOfSquares = Math.pow(Number(sideAB.value), 2) + Math.pow(Number(sideBC.value), 2);
-    
-    output.innerText = (Math.round(Math.sqrt(sumOfSquares) * 100) / 100).toFixed(2) + ' cm'
-    
-    
+    if (Number(sideAB.value) || Number(sideBC.value) < 0) {
+        output.innerText = 'Enter positive values!'
+    } else {
+        var sumOfSquares = Math.pow(Number(sideAB.value), 2) + Math.pow(Number(sideBC.value), 2);
+
+        output.innerText = (Math.round(Math.sqrt(sumOfSquares) * 100) / 100).toFixed(2) + ' cm'
+    }
+
+
+
 });
