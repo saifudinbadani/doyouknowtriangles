ab435d9e77810627eb76f75ded326e47589d00af
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-13 17:58:13 +0800

    negative number handling in angles page

--- angles.js
+++ angles.js
@@ -6,12 +6,16 @@ var output = document.querySelector('#output');
 
 
 
-checkButton.addEventListener('click',function checkSum(){
-    
+checkButton.addEventListener('click', function checkSum() {
+    if (Number(angleA.value) || Number(angleB.value) || Number(angleC.value) > 0) {
         if (Number(angleA.value) + Number(angleB.value) + Number(angleC.value) === 180) {
-            output.innerText='The angles entered does form a triangle.'
+            output.innerText = 'The angles entered form a triangle.'
         } else {
-            output.innerText='The angles entered does not form a triangle.'
+            output.innerText = 'The angles entered does not form a triangle.'
         }
-    
+        
+    } else {
+        output.innerText = 'Enter a positive number.'
+    }
+
 })
