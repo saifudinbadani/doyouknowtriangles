6491eafb953e56111e7d4f0e6370cb8ac4fb03c7
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-13 16:01:10 +0800

    added output in html

--- angles.js
+++ angles.js
@@ -2,15 +2,16 @@ var angleA = document.querySelector('#angleA');
 var angleB = document.querySelector('#angleB');
 var angleC = document.querySelector('#angleC');
 var checkButton = document.querySelector('#checkButton')
+var output = document.querySelector('#output');
 
 
 
 checkButton.addEventListener('click',function checkSum(){
     
         if (Number(angleA.value) + Number(angleB.value) + Number(angleC.value) === 180) {
-            console.log('The angles entered does form a triangle.')
+            output.innerText='The angles entered does form a triangle.'
         } else {
-            console.log('The angles entered does not form a triangle.')
+            output.innerText='The angles entered does not form a triangle.'
         }
     
 })
