acb1394a4f421c4c34cd4fcc444ad63917124c92
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-16 02:17:28 +0800

    updating js

--- hypotenuse.js
+++ hypotenuse.js
@@ -5,12 +5,13 @@ var output = document.querySelector('#output');
 
 
 submitButton.addEventListener('click', function checkHypotenuse() {
-    if (Number(sideAB.value) || Number(sideBC.value) < 0) {
-        output.innerText = 'Enter positive values!'
-    } else {
+    if (Number(sideAB.value) || Number(sideBC.value) > 0) {
         var sumOfSquares = Math.pow(Number(sideAB.value), 2) + Math.pow(Number(sideBC.value), 2);
 
         output.innerText = (Math.round(Math.sqrt(sumOfSquares) * 100) / 100).toFixed(2) + ' cm'
+        
+    } else {
+        output.innerText = 'Enter positive values!'
     }
 
 
